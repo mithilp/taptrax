53a9f0442f3db82cc1e0d5c02ca54a528fe1900d
mithilp <ping.mithil@gmail.com>
2024-02-18 05:44:22 -0800

    New drum sounds

--- src/utils/playDrum.ts
+++ src/utils/playDrum.ts
@@ -1,25 +1,16 @@
 "use client";
 
-import { DrumMachine } from "smplr";
-
-// @ts-ignore
-const context = new AudioContext();
-
-const drumMachine = new DrumMachine(context, { instrument: "TR-808" });
-drumMachine.output.setVolume(127);
-
 const playDrum = (music: Array<{ position: string; time: number }>) => {
-	const now = context.currentTime;
 	for (let i = 0; i < music.length; i++) {
 		setTimeout(() => {
-			drumMachine.start({
-				note:
-					music[i].position == "top"
-						? "snare"
-						: music[i].position == "bottom"
-						? "kick"
-						: "hihat-close",
-			});
+			const audio = new Audio(
+				music[i].position == "top"
+					? "/snare.wav"
+					: music[i].position == "bottom"
+					? "/kick.wav"
+					: "/hihat.wav"
+			);
+			audio.play();
 		}, music[i].time);
 	}
 };
