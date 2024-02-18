949cc6f184ba861a36c9019444a38c011852defe
mithilp <ping.mithil@gmail.com>
2024-02-17 18:07:58 -0800

    export to midi

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,23 +1,51 @@
 "use client";
 
+import MidiWriter from "midi-writer-js";
+
 export default function Page({ params }: { params: { id: string } }) {
 	const { id } = params;
 
 	const work = async () => {
-		// // cut video in halves
-		// await fetch(`/api/halves?id=${id}`, { method: "POST" });
+		// cut video in halves
+		await fetch(`/api/halves?id=${id}`, { method: "POST" });
 
-		// // get audio spikes
-		const spikes = [3129, 5203];
-		// await fetch(`/api/frames?id=${id}&frames=${spikes.join(",")}`, {
-		// 	method: "POST",
-		// });
-
-		await fetch(`/api/detect?id=${id}&frames=${spikes.join(",")}`, {
+		// get audio spikes
+		const spikes = [1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000];
+		await fetch(`/api/frames?id=${id}&frames=${spikes.join(",")}`, {
 			method: "POST",
 		});
 
-		alert("Got frames!");
+		const response = await fetch(
+			`/api/detect?id=${id}&frames=${spikes.join(",")}`,
+			{
+				method: "POST",
+			}
+		);
+		const json = await response.json();
+		console.log(json);
+
+		const track = new MidiWriter.Track();
+
+		track.addEvent(
+			json.map(
+				({ time, position }: { time: string; position: string }) =>
+					new MidiWriter.NoteEvent({
+						pitch:
+							position == "top" ? ["C1"] : position == "bottom" ? ["C2"] : "C3",
+						duration: "4",
+						tick: (Number(time) / 1000) * 512,
+					})
+			)
+		);
+
+		const output = new MidiWriter.Writer(track).dataUri();
+
+		const link = document.createElement("a");
+		link.download = "output.mid";
+		link.href = output;
+		document.body.appendChild(link);
+		link.click();
+		document.body.removeChild(link);
 	};
 
 	return (
--- src/app/api/detect/route.ts
+++ src/app/api/detect/route.ts
@@ -4,6 +4,7 @@ import { join } from "path";
 
 const detectImage = async (path: string) => {
 	const image = await jimp.read(path);
+
 	// @ts-ignore
 	await image.color([{ apply: "saturate", params: [100] }]);
 	await image.contrast(1);
@@ -25,6 +26,8 @@ const detectImage = async (path: string) => {
 };
 
 export async function POST(request: NextRequest) {
+	await new Promise((r) => setTimeout(r, 2000));
+
 	const searchParams = request.nextUrl.searchParams;
 	const id = searchParams.get("id") as string;
 	const frameStrings = searchParams.get("frames") as string;
@@ -33,19 +36,29 @@ export async function POST(request: NextRequest) {
 
 	console.log("started detecting in frames", id);
 
-	for await (const frame of frames) {
-		const dir = join("./", "tmp", id, frame) + "/";
+	const final = [];
 
-		if (await detectImage(dir + "bottom.jpg")) {
-			console.log("Detected in bottom.jpg", frame);
-		} else if (await detectImage(dir + "top.jpg")) {
-			console.log("Detected in top.jpg", frame);
+	for await (const time of frames) {
+		const dir = join("./", "tmp", id, time) + "/";
+
+		if (await detectImage("./" + dir + "bottom.jpg")) {
+			console.log("Detected in bottom.jpg", time);
+			final.push({
+				time: time,
+				position: "bottom",
+			});
+		} else if (await detectImage("./" + dir + "top.jpg")) {
+			console.log("Detected in top.jpg", time);
+			final.push({
+				time: time,
+				position: "top",
+			});
 		} else {
-			console.log("Not detected in", frame);
+			console.log("Not detected in", time);
 		}
 	}
 
-	console.log("finished detecting in frames", id);
+	console.log("finished detecting in frames", final);
 
-	return new Response("Success", { status: 200 });
+	return new Response(JSON.stringify(final), { status: 200 });
 }
